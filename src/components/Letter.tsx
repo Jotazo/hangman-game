d6803e5e4e7dbe4a9ec01b99dc8a72491ec77207
Javier Guerrero <javigh87@gmail.com>
2024-03-21 06:32:36 +0100

    feature: Letter created

---
+++ src/components/Letter.tsx
@@ -0,0 +1,27 @@
+import styles from "./Letter.module.css";
+
+interface LetterProps {
+  disabled?: boolean;
+  letter: string;
+  variant: "playable" | "keyboard";
+}
+
+const Letter = ({
+  letter,
+  variant = "playable",
+  disabled = false,
+}: LetterProps) => {
+  const showLetter = (disabled && variant === "keyboard") || !disabled;
+  return (
+    <div
+      className={`${styles.letterContainer} ${styles[variant]} ${
+        disabled && styles.disabled
+      }`}
+    >
+      <span className={`heading-l ${styles.letter}`}>
+        {showLetter && letter}
+      </span>
+    </div>
+  );
+};
+export default Letter;
