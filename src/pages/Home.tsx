f989d70ce0cac29785e13c83c32b8988e11b1503
Javier Guerrero <javigh87@gmail.com>
2024-03-21 11:08:03 +0100

    feature: Home page finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+
+import { ROUTES } from "../router/router";
+
 import HomeLayout from "../layouts/HomeLayout";
 
 import Button from "../components/Button";
@@ -9,8 +13,12 @@ const Home = () => {
   return (
     <HomeLayout>
       <Modal title={<HangmanTitle />}>
-        <PlayButton style={{ margin: "1rem 0" }} />
-        <Button text="HOW TO PLAY" variant="primary" />
+        <Link to={ROUTES.pickCategory}>
+          <PlayButton style={{ margin: "1rem 0" }} />
+        </Link>
+        <Link to={ROUTES.howToPlay}>
+          <Button text="HOW TO PLAY" variant="primary" />
+        </Link>
       </Modal>
     </HomeLayout>
   );
