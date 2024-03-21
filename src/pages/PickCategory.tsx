83eee041efbe8669dc4a1891992456904ac15e7f
Javier Guerrero <javigh87@gmail.com>
2024-03-21 17:20:14 +0100

    feature: PickCategory finished

--- src/pages/PickCategory.tsx
+++ src/pages/PickCategory.tsx
@@ -1,4 +1,32 @@
+import { Link } from "react-router-dom";
+
+import { ROUTES } from "../router/constants";
+
+import { getCategories } from "../utils/dataUtils";
+
+import BackHeader from "./Headers/BackHeader";
+
+import PickCategoryTitle from "../components/icons/PickCategoryTitle";
+
+import styles from "./PickCategory.module.css";
+
 const PickCategory = () => {
-  return <div>PickCategory</div>;
+  const categories = getCategories();
+  return (
+    <div className={`container d-flex flex-col`}>
+      <BackHeader title={<PickCategoryTitle />} />
+      <ul className={styles.list}>
+        {categories.map((category) => (
+          <Link
+            key={category}
+            style={{ textDecoration: "none" }}
+            to={`${ROUTES.game}?Category=${category}`}
+          >
+            <li className={styles.listItem}>{category}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default PickCategory;
