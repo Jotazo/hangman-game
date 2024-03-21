edad119e7f0e8781fb1f13d8dc877c13eb620cf7
Javier Guerrero <javigh87@gmail.com>
2024-03-21 08:50:02 +0100

    feature: Layout created

---
+++ src/Layout.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+
+import { checkDevice } from "./utils/checkDevice";
+
+import bgMobile from "./assets/images/background-mobile.png";
+import bgTablet from "./assets/images/background-tablet.png";
+import bgDesktop from "./assets/images/background-desktop.png";
+
+import styles from "./Layout.module.css";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  const device = checkDevice();
+
+  const bgToShow =
+    device === "mobile" ? bgMobile : device === "tablet" ? bgTablet : bgDesktop;
+
+  return (
+    <div className={styles.layout}>
+      <img
+        src={bgToShow}
+        alt="background"
+        className={styles.layoutBackground}
+      />
+      {children}
+    </div>
+  );
+};
+export default Layout;
---
+++ src/utils/checkDevice.ts
@@ -0,0 +1,20 @@
+type Devices = "mobile" | "tablet" | "desktop";
+
+export const checkDevice = (): Devices => {
+  const isMobile = window.matchMedia(
+    "only screen and (min-width : 380px) and (max-width : 640px)"
+  ).matches;
+
+  const isTablet = window.matchMedia(
+    "only screen and (min-width : 641px) and (max-width : 1280px)"
+  ).matches;
+
+  const isDesktop = window.matchMedia(
+    "only screen and (min-width : 1281px)"
+  ).matches;
+
+  if (!isMobile && !isTablet && !isDesktop)
+    throw new Error("Unsuported Device");
+
+  return isMobile ? "mobile" : isTablet ? "tablet" : "desktop";
+};
