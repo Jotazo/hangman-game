63a823b850d6dab97768173c8ceb1b1cc700d2f9
Javier Guerrero <javigh87@gmail.com>
2024-03-21 11:34:24 +0100

    fix: Removed Layout from page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
-import { ROUTES } from "../router/router";
-
-import HomeLayout from "../layouts/HomeLayout";
+import { ROUTES } from "../router/constants";
 
 import Button from "../components/Button";
 import HangmanTitle from "../components/icons/HangmanTitle";
@@ -11,16 +9,14 @@ import PlayButton from "../components/PlayButton";
 
 const Home = () => {
   return (
-    <HomeLayout>
-      <Modal title={<HangmanTitle />}>
-        <Link to={ROUTES.pickCategory}>
-          <PlayButton style={{ margin: "1rem 0" }} />
-        </Link>
-        <Link to={ROUTES.howToPlay}>
-          <Button text="HOW TO PLAY" variant="primary" />
-        </Link>
-      </Modal>
-    </HomeLayout>
+    <Modal title={<HangmanTitle />}>
+      <Link to={ROUTES.pickCategory}>
+        <PlayButton style={{ margin: "1rem 0" }} />
+      </Link>
+      <Link to={ROUTES.howToPlay}>
+        <Button text="HOW TO PLAY" variant="primary" />
+      </Link>
+    </Modal>
   );
 };
 export default Home;
