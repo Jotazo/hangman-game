c0a244a0bead9e123f3df551d46d51a1e37701c2
Javier Guerrero <javigh87@gmail.com>
2024-03-23 22:17:36 +0100

    feature: gameHeader handles life counter

--- src/pages/Headers/GameHeader.tsx
+++ src/pages/Headers/GameHeader.tsx
@@ -2,13 +2,43 @@ import { IconButtonMenu, IconHeart } from "@/components/icons";
 
 import styles from "./GameHeader.module.css";
 
-const GameHeader = ({ title }: { title: string }) => {
+const getLifeColor = (errors: number) => {
+  const COLORS = {
+    default: "#162676", // blue navy
+    warning: "CC5500", // orange
+    error: "#FF0000", // red
+  };
+
+  return errors >= 5
+    ? COLORS.error
+    : errors >= 3
+    ? COLORS.warning
+    : COLORS.default;
+};
+
+const GameHeader = ({
+  title,
+  errors,
+  onOpenMenu,
+}: {
+  title: string;
+  errors: number;
+  onOpenMenu: () => void;
+}) => {
+  const lifeColor = getLifeColor(errors);
+
   return (
     <header className={styles.header}>
-      <IconButtonMenu />
+      <IconButtonMenu onClick={onOpenMenu} />
       <h3 className={styles.title}>{title}</h3>
       <div className={styles.lifeContainer}>
-        <div className={styles.life}></div>
+        <div
+          className={styles.life}
+          style={{
+            width: `${100 - 12.5 * errors}%`,
+            backgroundColor: lifeColor,
+          }}
+        ></div>
       </div>
       <IconHeart />
     </header>
