f69d12609894a3aa5d770e3d926e2455ef20b1f5
Javier Guerrero <javigh87@gmail.com>
2024-03-23 20:03:20 +0100

    refactor: Modal moved to folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Letter } from "./Letter";
-export { default as Modal } from "./Modal";
+export { default as Modal } from "./Modal/Modal";
+export { default as ModalContainer } from "./Modal/ModalContainer";
 export { default as PlayButton } from "./PlayButton";
