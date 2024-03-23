756f446273807e4749c397aa54fef3d347710474
Javier Guerrero <javigh87@gmail.com>
2024-03-23 22:18:21 +0100

    feature: Game page handles keys & pause modal

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,43 +1,65 @@
 import { useState } from "react";
 
-import { Navigate, useSearchParams } from "react-router-dom";
+import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
 
 import { ROUTES } from "../../router/constants";
 
-import { getRandomWord, isValidCategory } from "@/data";
+import { isValidCategory } from "@/data";
+
+import useGameInfo from "@/hooks/useGameInfo";
 
 import GameHeader from "../Headers/GameHeader";
 import ScreenWord from "./ScreenWord";
 import Keyboard from "./Keyboard";
 
+import { Button, ModalContainer } from "@/components";
+import PausedTitle from "@/components/icons/PausedTitle";
+
 import styles from "./Game.module.css";
 
 const Game = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const categorySelected = searchParams.get("Category");
 
-  const [rndWord, setRndWord] = useState(() => {
-    if (!isValidCategory(categorySelected))
-      return { name: "", selected: false };
-    return getRandomWord(categorySelected);
-  });
-
-  const [keysPressed, setKeysPressed] = useState<string[]>([]);
+  const { randomAnswer, keysPressed, errors, onClickLetter } =
+    useGameInfo(categorySelected);
 
   if (!isValidCategory(categorySelected)) return <Navigate to={ROUTES.home} />;
 
-  const onClickLetter = (letter: string) => {
-    if (keysPressed.includes(letter)) return;
-    setKeysPressed((prevKeys) => [...prevKeys, letter]);
-  };
-
   return (
     <div className="container">
-      <GameHeader title={categorySelected} />
+      <GameHeader
+        title={categorySelected}
+        errors={errors}
+        onOpenMenu={() => setIsModalOpen(true)}
+      />
       <main className={styles.main}>
-        <ScreenWord rndWord={rndWord} keysPressed={keysPressed} />
+        <ScreenWord randomAnswer={randomAnswer} keysPressed={keysPressed} />
         <Keyboard keysPressed={keysPressed} onClickLetter={onClickLetter} />
       </main>
+      {isModalOpen && (
+        <ModalContainer title={<PausedTitle />} sxTitle={{ top: "-30px" }}>
+          <Button
+            variant="primary"
+            text="CONTINUE"
+            onClick={() => setIsModalOpen(false)}
+          />
+          <Button
+            variant="primary"
+            text="NEW CATEGORY"
+            onClick={() => navigate(ROUTES.pickCategory)}
+          />
+          <Button
+            variant="secondary"
+            text="QUIT GAME"
+            onClick={() => navigate(ROUTES.home)}
+          />
+        </ModalContainer>
+      )}
     </div>
   );
 };
--- src/pages/Game/ScreenWord.tsx
+++ src/pages/Game/ScreenWord.tsx
@@ -1,5 +1,3 @@
-import { Word } from "@/data";
-
 import { generateRandomID } from "@/utils";
 
 import { Letter } from "@/components";
@@ -7,15 +5,15 @@ import { Letter } from "@/components";
 import styles from "./ScreenWord.module.css";
 
 const ScreenWord = ({
-  rndWord,
+  randomAnswer,
   keysPressed,
 }: {
-  rndWord: Word;
+  randomAnswer: string;
   keysPressed: string[];
 }) => {
   return (
     <section className={styles.gameScreen}>
-      {rndWord.name.split(" ").map((word) => {
+      {randomAnswer.split(" ").map((word) => {
         return (
           <div key={generateRandomID(12)} className="d-flex">
             {word.split("").map((letter) => (
